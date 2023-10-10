e9ffcd928f438b172e852f6330517aaa82224bcd
Victor Franco <victorf05.dev@gmail.com>
2023-10-10 06:39:45 -0300

    Loading component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import fetchProducts from '../../api/fetchProducts';
+import ProductCard from '../ProductCard/ProductCard';
+import Loading from '../Loading/Loading';
 
 import './Products.css';
-import ProductCard from '../ProductCard/ProductCard';
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
 
     fetchProducts('iphone').then((response) => {
       setProducts(response);
+      setLoading(false);
     });
 
   }, []);
 
   return(
-    <section className="products container">
-      {products.map((product) => <ProductCard key={product.id} data={product} />)}
-    </section>
+    (loading && <Loading />) || (
+      <section className="products container">
+        {products.map((product) => <ProductCard key={product.id} data={product} />)}
+      </section>
+    )
   );
 }
 
